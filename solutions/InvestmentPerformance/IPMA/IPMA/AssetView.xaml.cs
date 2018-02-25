d718c5a5aa0a7b370737ae8ad84cf39e78c24d61
Samuel Cooney <cooneys@oregonstate.edu>
2018-02-25 15:51:40 -0800

    Add fully designed login and signup page, as well as a temp asset view that can be transformed into dashboard view as well. Having errors with depolying

---
+++ solutions/InvestmentPerformance/IPMA/IPMA/AssetView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IPMA {
+	public partial class AssetView : ContentPage {
+        public AssetView () {
+            InitializeComponent ();
+
+
+        }
+	}
+}
