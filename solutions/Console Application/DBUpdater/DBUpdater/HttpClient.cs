2d5ef140e3ce2ec93ae391620d7475da2ef327d1
tjones0517 <jonesty@oregonstate.edu>
2018-03-13 02:58:40 -0700

    pushing prelim console changes

---
+++ solutions/Console Application/DBUpdater/DBUpdater/HttpClient.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+namespace HttpClientSample
+{
+    public class Tickersymbol
+    {
+        //public string Id { get; set; }
+        public string Name { get; set; }
+       // public decimal Price { get; set; }
+        //public string Category { get; set; }
+    }
+
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+
+        static void ShowProduct(Tickersymbol product)
+        {
+            Console.WriteLine($"Name: {product.Name}");
+        }
+
+        static async Task<Uri> CreateProductAsync(Tickersymbol product)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "api/products", product);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        static async Task<Tickersymbol> GetProductAsync(string path)
+        {
+            Tickersymbol product = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                product = await response.Content.ReadAsAsync<Tickersymbol>();
+            }
+            return product;
+        }
+
+        /*static async Task<Tickersymbol> UpdateProductAsync(Tickersymbol product)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync(
+                $"api/products/{product.Id}", product);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated product from the response body.
+            product = await response.Content.ReadAsAsync<Tickersymbol>();
+            return product;
+        }*/
+
+        static async Task<HttpStatusCode> DeleteProductAsync(string id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync(
+                $"api/products/{id}");
+            return response.StatusCode;
+        }
+
+        /*static void Main()
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }*/
+
+        static async Task RunAsync()
+        {
+            // Update port # in the following line.
+            client.BaseAddress = new Uri("http://localhost:64195/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                // Create a new product
+                Tickersymbol product = new Tickersymbol
+                {
+                    Name = "Gizmo",
+                    //Price = 100,
+                    //Category = "Widgets"
+                };
+
+                //var url = await CreateProductAsync(product);
+                //Console.WriteLine($"Created at {url}");
+
+                // Get the product
+                product = await GetProductAsync("http://web.engr.oregonstate.edu/~jonesty/api.php/Investments");
+                ShowProduct(product);
+
+                // Update the product
+                //Console.WriteLine("Updating price...");
+                //product.Price = 80;
+                //await UpdateProductAsync(product);
+
+                // Get the updated product
+                //product = await GetProductAsync(url.PathAndQuery);
+                //ShowProduct(product);
+
+                // Delete the product
+                //var statusCode = await DeleteProductAsync(product.Id);
+               // Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ solutions/Console Application/DBUpdater/DBUpdater/TryNumber2.cs
@@ -0,0 +1,124 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+
+    public static List<string> tickers;
+    public static List<string> tickerList;
+
+    public class CompanyInfo
+    {
+        public string tickersymbol { get; set; }
+    }
+
+
+    static void Main(string[] args)
+    {
+        Task T = new Task(FetchTickers);
+        T.Start();
+        Console.WriteLine("Json data........");
+        Console.ReadLine();
+    }
+    static async void GetTickers()
+    {
+        List<string> tickers = new List<string>();
+        using (var client = new HttpClient())
+        {
+
+            HttpResponseMessage response = await client.GetAsync("http://web.engr.oregonstate.edu/~jonesty/api.php/Investments");
+
+            response.EnsureSuccessStatusCode();
+
+            using (HttpContent content = response.Content)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+
+
+                var articles = JsonConvert.DeserializeObject<List<CompanyInfo>>(responseBody);
+
+
+
+                foreach (var Emp in articles)
+                {
+                    Console.WriteLine("{0}\t", Emp.tickersymbol);
+                    tickers.Add(Emp.tickersymbol);
+                }
+
+            }
+
+        }
+    }
+    static async void GetBalanceSheet(){
+        using (var client = new HttpClient())
+        {
+
+            HttpResponseMessage response = await client.GetAsync("http://web.engr.oregonstate.edu/~jonesty/api.php/Investments");
+
+            response.EnsureSuccessStatusCode();
+
+            using (HttpContent content = response.Content)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+
+
+                var articles = JsonConvert.DeserializeObject<List<CompanyInfo>>(responseBody);
+
+
+
+                foreach (var Emp in articles)
+                {
+                    Console.WriteLine("{0}\t", Emp.tickersymbol);
+                    tickers.Add(Emp.tickersymbol);
+                }
+
+            }
+
+        }
+    }
+
+    static async void FetchTickers()
+    {
+        var client = new HttpClient();
+        List<string> tickerList = new List<string>();
+        string Url = "http://web.engr.oregonstate.edu/~jonesty/api.php/Investments";
+
+        string content = await client.GetStringAsync(Url);
+        JArray tickerlistC = JArray.Parse(content);
+
+
+        if (tickerlistC.Count == 0)
+        {
+            Console.WriteLine("null returned");
+            //return null;
+            //Debug.WriteLine("null returned");
+        }
+
+        else
+        {
+            for (var i = 0; i < tickerlistC.Count; i++)
+
+            {
+                //Debug.WriteLine((string)portfolios[i]["username"]);
+
+                tickerList.Add((string)tickerlistC[i]["tickersymbol"]);
+                Console.Write(tickerList[i]);
+
+
+            }
+            //Console.Write()
+            //return tickerList;
+
+        }
+
+    }
+
+}
+
