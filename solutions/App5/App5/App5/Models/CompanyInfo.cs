cce0dbe9b20f434117af312501396a03f14dc5df
tjones0517 <jonesty@oregonstate.edu>
2018-03-21 21:24:06 -0700

    pushing company info

---
+++ solutions/App5/App5/App5/Models/CompanyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App5.Models
+{
+    public class CompanyInfo
+    {
+        public string tickersymbol { get; set; }
+        public double currentprice { get; set; }
+
+        public CompanyInfo() { }
+
+        public CompanyInfo(string tckr, double currprice)
+        {
+            this.tickersymbol = tckr;
+            this.currentprice = currprice;
+
+        }
+    }
+
+}
