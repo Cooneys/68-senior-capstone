0456f8a1837bd9762999250ad2e2ac8df59d6c65
Avi Sinha <avisinha0@gmail.com>
2018-05-02 14:38:25 -0700

    Asset Details page complete

--- solutions/App5/App5/App5/Models/CompanyInfo.cs
+++ solutions/App5/App5/App5/Models/CompanyInfo.cs
@@ -8,6 +8,21 @@ namespace App5.Models
     {
         public string tickersymbol { get; set; }
         public double currentprice { get; set; }
+        public double InventoryTurnover { get; set; }
+        public double ReturnOnAssets { get; set; }
+        public double ReturnOnEquity { get; set; }
+        public double EBTMargin { get; set; }
+        public double AssetTurnover { get; set; }
+        public double ReceivablesTurnover { get; set; }
+        public double NetIncome { get; set; }
+        public double EarningsPerShare { get; set; }
+        public double InterestCoverage { get; set; }
+        public double TotalCurrentAssets { get; set; }
+        public double TaxRate {get; set;}
+        public double FreeCashFlow { get; set; }
+        public double Revenue { get; set; }
+
+
 
         public CompanyInfo() { }
 
